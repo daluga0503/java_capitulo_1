a78a5dfbf6b533645ae8adc5bdfe1d267f9f64f3
daluga0503 <daniel.luquegallardo@gmail.com>
2022-10-10 10:35:29 +0200

    ejercicio 3 realizado

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,3 +1,14 @@
 public class Ejercicio3 {
-    
+    public static void main(String[] args){
+        System.out.printf( "%-10s %s\n",  "football", "fútbol");
+        System.out.printf( "%-10s %s\n",  "dog", "perro");
+        System.out.printf( "%-10s %s\n",  "table", "mesa");
+        System.out.printf( "%-10s %s\n",  "door", "puerta");
+        System.out.printf( "%-10s %s\n",  "phone", "móvil");
+        System.out.printf( "%-10s %s\n",  "case", "funda");
+        System.out.printf( "%-10s %s\n",  "rice", "arroz");
+        System.out.printf( "%-10s %s\n",  "chicken", "pollo");
+        System.out.printf( "%-10s %s\n",  "ball", "pelota");
+        System.out.printf( "%-10s %s\n",  "boots", "botas");
+}
 }
