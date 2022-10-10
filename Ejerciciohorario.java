49aa88716438fb14c76173c8c242972144e1697e
daluga0503 <daniel.luquegallardo@gmail.com>
2022-10-10 10:17:32 +0200

    ejercicio 3 hecho

---
+++ Ejerciciohorario.java
@@ -0,0 +1,7 @@
+public class Ejerciciohorario{
+    public static void main(String[] args){
+        System.out.println("Este es tu horario del instituto") ;
+        System.out.print("     Lunes     Martes     Miércoles     Jueves     Viernes     ") ;
+        System.out.print("      Progra     Progra     Progra       Progra      Fol");
+    }
+}
