f5b8173bdc8278881215fb664d23758465c8e0c6
daluga0503 <daniel.luquegallardo@gmail.com>
2022-10-11 10:42:21 +0200

    horario realizado

---
+++ Ejercicio4.java
@@ -0,0 +1,12 @@
+public class Ejercicio4 {
+    public static void main(String[] args){
+        System.out.println("\n Lunes\tMartes\tMiérc\tJueves.\tViernes");
+        System.out.println(" ======\t=======\t======\t=======\t=======");
+        System.out.println(" PROGRA\tSIS\tENTDE\tPROGR\tFOL");
+        System.out.println(" PROGRA\tSIS\tENTDE\tPROGRA\tFOL");
+        System.out.println(" LYM\tSIS\tPROG\tLYM\tFOL");
+        System.out.println(" LYM\tPROGRA\tPROG\tLYM\tSIS");
+        System.out.println(" BBDD\tPROGRA\tBBDD\tBBDD\tSIS");
+        System.out.println(" BBDD\tENTDE\tBBDD\tBBDD\tSIS");
+    }
+}
